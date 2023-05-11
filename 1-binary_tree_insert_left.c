dc1f5c531608f64f57f674f1077d542663a523c6
RM92023 <robinjara20@gmail.com>
2023-05-11 16:15:45 -0500

    updating file 1.binary-tree-insert.left.c in parhentesis in return

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,10 +12,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 binary_tree_t *new_node = NULL;
 if (parent == NULL)
-(return NULL);
+return (NULL);
 new_node = malloc(sizeof(binary_tree_t));
 if (new_node == NULL)
-(return NULL);
+return (NULL);
 new_node->n = value;
 new_node->parent = parent;
 new_node->right = NULL;
@@ -30,5 +30,5 @@ new_node->left = parent->left;
 parent->left = new_node;
 new_node->left->parent = new_node;
 }
-(return new_node);
+return (new_node);
 }
