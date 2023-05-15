b13de4d04f7a628d102a8e0f4565c17af30133c7
RM92023 <robinjara20@gmail.com>
2023-05-15 09:14:55 -0500

    adding binray_Trees.h, delete struct because it is repeat

--- binary_trees.h
+++ binary_trees.h
@@ -20,8 +20,6 @@ struct binary_tree_s
     struct binary_tree_s *left;
     struct binary_tree_s *right;
 };
-
-typedef struct binary_tree_s binary_tree_t;
 /*Basic Binary Tree*/
 typedef struct binary_tree_s binary_tree_t;
 
