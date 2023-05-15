42a7e1f55809b61ce19ad94653be93287d57d7a0
RM92023 <robinjara20@gmail.com>
2023-05-15 08:55:42 -0500

    Adding 2-binary-tree-insert-right and spaces and tabs in code

--- binary_tree_print.c
+++ binary_tree_print.c
@@ -10,9 +10,10 @@
  */
 void binary_tree_print(const binary_tree_t *tree)
 {
-if (tree == NULL)
-return;
-binary_tree_print(tree->left);
-printf("%d\n", tree->n);
-binary_tree_print(tree->right);
+	if (tree == NULL)
+	return;
+
+	binary_tree_print(tree->left);
+	printf("%d\n", tree->n);
+	binary_tree_print(tree->right);
 }
